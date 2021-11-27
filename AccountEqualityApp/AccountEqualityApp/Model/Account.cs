80a374d72f333af9adfd7a584573924fd16d09f6
Nelson Dsouza <nelsondsouza458@gmail.com>
2021-11-27 09:29:53 +0530

    classwork1

---
+++ AccountEqualityApp/AccountEqualityApp/Model/Account.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountEncapsulationApp.Model
+{
+    class Account
+    {
+        private String _name;
+        private int _accountNumber;
+        private double _balance;
+
+        public Account(String name , int accountNumber , double balance)
+        {
+            _name = name;
+            _accountNumber = accountNumber;
+            _balance = balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance = _balance + amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            _balance = _balance - amount;
+
+            if (_balance < 500)
+            {
+                Console.WriteLine("The minimum amount of 500 must be there in the bank");
+                Console.WriteLine("");
+                _balance = _balance + amount;
+            }
+        }
+
+        public override String ToString()
+        {
+            String output = "The name is " + this._name + "Having balance " + Convert.ToString(this._balance) + "Account no: " + Convert.ToString(this._accountNumber); 
+            return output +" \n Parent ->"+base.ToString();
+        }
+
+        public override bool Equals(Account acc1, Account acc2)
+        {
+            acc1.Equals(acc2) = true;
+            return acc1.Equals(acc2) ;
+        }
+
+
+
+        public String Getname()
+        {
+            return _name;
+        }
+
+        public double GetAccountNumber()
+        {
+            return _accountNumber;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+}
---
+++ AccountEqualityApp/AccountEqualityApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountEncapsulationApp.Model;
+
+namespace AccountEqualityApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CaseStudy2();
+            Console.ReadLine();
+        }
+
+        public static void CaseStudy1()
+        {
+            Account acc1 = new Account("Nelson", 101, 10000);
+            Console.WriteLine(acc1);
+            Console.WriteLine(acc1.ToString());
+        }
+
+        public static void CaseStudy2()
+        {
+            Account acc1 = new Account("Nelson", 101, 10000);
+            Account acc2 = new Account("Nelson", 101, 10000);
+            Console.WriteLine(acc1.GetHashCode());
+            Console.WriteLine(acc2.GetHashCode());
+            Console.WriteLine(acc1 == acc2);
+            Console.WriteLine(acc1.Equals(acc2));
+
+        }
+    }
+}
