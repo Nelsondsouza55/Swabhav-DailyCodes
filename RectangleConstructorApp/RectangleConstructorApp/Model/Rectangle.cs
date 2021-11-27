6f33fce8b08fe70a1aee23d8a19902ca70cf7503
Nelson Dsouza <nelsondsouza458@gmail.com>
2021-11-27 10:05:29 +0530

    classwork

---
+++ RectangleConstructorApp/RectangleConstructorApp/Model/Rectangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleConstructorApp.Model
+{
+
+    class Rectangle
+    {
+        private readonly int _width;
+        private readonly int _height;
+        private readonly String _color;
+        private readonly String _borderColor;
+
+        public Rectangle(int width, int height, String color)
+        {
+            _width = width;
+            _height = height;
+            _color = color;
+
+        }
+
+        public Rectangle(int width, int height, String color, String borderColor)
+        {
+            _width = width;
+            _height = height;
+            _color = color;
+            _borderColor = borderColor;
+
+        }
+
+            public int GetWidth()
+            {
+                return _width;
+            }
+
+            public int GetHeight()
+            {
+                return _height;
+            }
+
+            public String GetColor()
+            {
+                return _color;
+            }
+
+            public String GetBorderColor()
+             {
+            return _borderColor;
+             }
+
+        public int GetArea()
+            {
+                return _width * _height;
+            }
+        
+    }
+}
---
+++ RectangleConstructorApp/RectangleConstructorApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RectangleConstructorApp.Model;
+
+namespace RectangleConstructorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(50, 50, "blue" , "green");
+            PrintDetails(r1);
+            Console.WriteLine("");
+            CaseStudy1();
+            Console.ReadLine();
+
+
+        }
+
+
+        public static void CaseStudy1()
+        {
+            Rectangle r1 = new Rectangle(100, 50, "red");
+            PrintDetails(r1);
+        }
+
+        public static void PrintDetails(Rectangle r)
+        {
+            Console.WriteLine("The width is " +r.GetWidth());
+            Console.WriteLine("The height is " + r.GetHeight());
+            Console.WriteLine("The color is " + r.GetColor());
+            Console.WriteLine("The border color is " + r.GetBorderColor());
+
+        }
+    }
+}
