34130ccf225a64f23a634c8e6169f756f3b236d8
Nelson Dsouza <nelsondsouza458@gmail.com>
2021-11-27 10:09:54 +0530

    classwork

---
+++ RectangleEnumApp/RectangleEnumApp/Model/Rectangle.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RectangleEnumApp.Model
+{
+    
+
+
+
+    class Rectangle
+    {
+        private int _width;
+        private int _height;
+        private ColorType _borderColor = ColorType.RED;
+
+        public Rectangle(int width , int height  )
+        {
+            _width = width;
+            _height = height;
+        }
+
+
+
+        public int Width
+        {
+            set
+            {
+                if (value < 100)
+                {
+                    _width = value;
+                }
+                else
+                {
+                    _width = 100;
+                }
+            }
+
+            get
+            {
+                return _width;
+            }
+
+        }
+
+        public int Height
+        {
+            set
+            {
+                if (value < 100)
+                {
+                    _height = value;
+                }
+                else
+                {
+                    _height = 100;
+                }
+            }
+
+            get
+            {
+                return _height;
+            }
+
+        }
+
+
+
+        public int CalculateArea
+        {
+            get
+            {
+                return _width * _height;
+            }
+        }
+
+
+
+        public ColorType BorderColor
+        {
+            set
+            {
+                _borderColor = value;
+            }
+
+            get
+            {
+                return _borderColor;
+            }
+           
+        }
+
+        
+            
+    }
+}
---
+++ RectangleEnumApp/RectangleEnumApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RectangleEnumApp.Model;
+
+namespace RectangleEnumApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(0,0);
+            
+            r1.Width = 101;
+            r1.Height = 20;
+            r1.BorderColor = ColorType.BLUE;
+            PrintDetails(r1);
+
+            Console.ReadLine();
+
+        }
+
+
+        public static void PrintDetails(Rectangle r)
+        {
+            Console.WriteLine("The width is {0}" , r.Width );
+            Console.WriteLine("The height is {0}", r.Height);
+            Console.WriteLine("The border color is {0}", r.BorderColor);
+        }
+    }
+}
