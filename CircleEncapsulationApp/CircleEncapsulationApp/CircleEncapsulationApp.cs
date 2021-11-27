87e2240b23eb72bc136364a6a3c654b3f2381fe2
Nelson Dsouza <nelsondsouza458@gmail.com>
2021-11-27 09:54:29 +0530

    classwork

---
+++ CircleEncapsulationApp/CircleEncapsulationApp/CircleEncapsulationApp.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CircleEncapsulationApp.Model;
+
+namespace CircleEncapsulationApp
+{
+    class CircleEncapsulationApp
+    {
+        static void Main(string[] args)
+        {
+            CaseStudy1();
+            CaseStudy2();
+            CaseStudy3();
+            Console.ReadLine();
+        }
+
+
+        static void CaseStudy3()
+        {
+            Circle c1 = new Circle();
+            c1.SetRadius(6);
+            c1.SetFillColor("green");
+            c1.SetBorderColor("red");
+
+            Circle c2 = new Circle();
+            c2.SetRadius(9);
+            c2.SetFillColor("blue");
+            c2.SetBorderColor("red");
+
+            Circle[] manyCircles = new Circle[3];
+            manyCircles[0] = c1;
+            manyCircles[1] = new Circle();
+            manyCircles[1].SetRadius(7);
+            manyCircles[1].SetBorderColor("red");
+            manyCircles[1].SetFillColor("blue");
+            manyCircles[2] = c2;
+
+            PrintDetails(manyCircles);
+            PrintTheBiggestCircle(manyCircles);
+
+
+
+
+
+
+
+
+
+        }
+
+
+        static void CaseStudy2()
+        {
+
+            Console.WriteLine("Area of the anonymous circle is {0}",new Circle().CalculateArea());
+            Console.WriteLine("Border color of the anonymous circle is {0}", new Circle().GetBorderColor());
+            PrintDetails(new Circle());
+
+        }
+        static void CaseStudy1()
+        {
+            Circle c1 = new Circle();
+
+            PrintDetails(c1);
+
+            c1.SetRadius(5.5f);
+            c1.SetBorderColor("blue");
+            c1.SetFillColor("green");
+
+            PrintDetails(c1);
+            
+        }
+
+        static void PrintDetails(Circle c)
+        {
+            Console.WriteLine("area is {0}", c.CalculateArea());
+
+            Console.WriteLine("Radius is {0} border color is {1} fill color is {2}", c.GetRadius(),
+                c.GetBorderColor(), c.GetFillColor());
+
+            Console.WriteLine("");
+
+        }
+
+        static void PrintDetails(Circle[] circles)
+        {
+            foreach (Circle c in circles)
+            {
+                PrintDetails(c);
+            }
+
+        }
+
+        static void PrintTheBiggestCircle(Circle[] l)
+        {
+            float max = 0.0f;
+            for (int i = 0; i < l.Length; i++)
+            {
+              
+                foreach (Circle cir  in l)
+                {
+                    if (cir.CalculateArea() > max)
+                    {
+                        max = cir.CalculateArea();
+                    }
+
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("The biggest circle is " +max);
+        }
+    }
+}
---
+++ CircleEncapsulationApp/CircleEncapsulationApp/Model/Circle.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleEncapsulationApp.Model
+{
+    class Circle
+    {
+        private float _radius ;
+        private String _borderColor ;
+        private String _fillColor;
+        
+
+
+
+        public void SetRadius(float radius)
+        {
+            if (0 < radius && radius > 11)
+            {
+                radius = 1.0f;
+
+            }
+
+            _radius = radius;
+
+            
+        }
+
+        public void SetBorderColor(String borderColor)
+        {
+            if (borderColor != "red" && borderColor != "blue" && borderColor != "green")
+            {
+                Console.WriteLine("Invalid color");
+            }
+            else
+            {
+                _borderColor = borderColor;
+            }
+
+           
+        }
+
+
+        public void SetFillColor(String fillColor)
+        {
+            if (fillColor != "red" && fillColor != "blue" && fillColor != "green")
+            {
+                Console.WriteLine("Invalid color");
+
+            }
+            else
+            {
+                _fillColor = fillColor;
+            }
+
+            
+
+
+        }
+
+        public float GetRadius()
+        {
+            return _radius;
+        }
+
+        public String GetBorderColor()
+        {
+            return _borderColor;
+        }
+
+        public String GetFillColor()
+        {
+            return _fillColor;
+        }
+
+        public float CalculateArea()
+        {
+            return 3.14f * _radius * _radius;
+        }
+    }
+}
