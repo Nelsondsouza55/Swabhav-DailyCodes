46c44f55ece625fcd2048d3791ec8baaf4a1b742
Nelson Dsouza <nelsondsouza458@gmail.com>
2021-11-27 09:49:22 +0530

    assignment

---
+++ ConsoleAurionProMathCalculator/ConsoleAurionProMathCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AurionProMathCalculator;
+
+namespace ConsoleAurionProMathCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            Console.WriteLine(calc.PrimeNoCheck(52));
+            //calc.GenerateEvenNo(30);
+            Console.WriteLine("");
+            calc.GeneratePrime(10);
+            Console.ReadLine();
+        }
+    }
+}
