62710e4e0c730b649bf4c886f5c8009b59a2fb39
Nelson Dsouza <nelsondsouza458@gmail.com>
2021-11-27 10:07:38 +0530

    classwork

---
+++ RectangleEncapsulationApp/RectangleEncapsulationApp/Model/Rectangle.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleEncapsulationApp.Model
+{
+    class Rectangle
+    {
+        private int pwidth;
+        private int pheight;
+
+
+        public void SetWidth(int width)
+        {
+
+            if (width < 0)
+            {
+                width = 1;
+            }
+
+            else if (width > 100)
+            {
+                width = 100;
+            }
+
+            pwidth = width;
+        }
+
+        public void SetHeight(int height)
+        {
+
+            if (height < 0)
+            {
+                height = 1;
+            }
+
+            else if (height > 100)
+            {
+                height = 100;
+            }
+
+           pheight = height;
+        }
+
+
+        public int GetHeight()
+        {
+            return pheight;
+        }
+
+        public int GetWidth()
+        {
+            return pwidth;
+        }
+    }
+}
+
+
+           
+
---
+++ RectangleEncapsulationApp/RectangleEncapsulationApp/RectangleEncapsulationApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RectangleEncapsulationApp.Model;
+
+namespace RectangleEncapsulationApp
+{
+    class RectangleEncapsulationApp
+    {
+        static void Main(string[] args)
+        {
+            Rectangle small = new Rectangle();
+            small.SetWidth(-10);
+            small.SetHeight(100);
+            Console.WriteLine(small.GetWidth());
+            Console.WriteLine(small.GetHeight());
+            Console.ReadLine();
+
+        }
+
+    }
+}
