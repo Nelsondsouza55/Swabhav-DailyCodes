8479522556d1c1b3e66cf4e208c116ccdcfb5ab7
Nelson Dsouza <nelsondsouza458@gmail.com>
2021-11-27 09:36:27 +0530

    assignment

---
+++ AnotherProjectApp/AnotherProjectApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AurionProMathCalculator;
+
+namespace AnotherProjectApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calc = new Calculate();
+            double num1 = 170;
+            double num2 = 120;
+            double result = calc.Addition(num1, num2);
+            Console.Write(num1);
+            Console.Write(" + ");
+            Console.Write(num2);
+            Console.Write(" = ");
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
